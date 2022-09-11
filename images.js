d742593f408d6e38fcda16631760d5f7cea7dd02
valentinnaa <112026828+valentinnaa@users.noreply.github.com>
2022-09-11 14:34:47 +0400

    Add files via upload

---
+++ images.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+app.use(express.static('images'))
+app.use('/images', express.static('images'))
+app.listen(3000, function(req,res)
+{ console.log('running')
+})
